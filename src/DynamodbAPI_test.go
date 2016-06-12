044674d7bc007ac174897a9c813ebbf5cd651d9e
Johnzeng <Johnz@misfit.com>
2016-06-12 16:57:54 +0800

    correct the query map, it's working now.

--- src/DynamodbAPI_test.go
+++ src/DynamodbAPI_test.go
@@ -7,24 +7,18 @@ import "github.com/aws/aws-sdk-go/aws/session"
 import "github.com/aws/aws-sdk-go/aws"
 
 func TestDynamoGetTableAPI(t *testing.T) {
-	config := aws.NewConfig().WithRegion("us-east-1").WithEndpoint("http://127.0.0.1:8000")
 
-	db := dynamodb.New(session.New(), config)
+	db := dynamodb.New(session.New(
+		&aws.Config{
+			Region:   aws.String("us-east-1"),
+			Endpoint: aws.String("http://127.0.0.1:8000")}))
 
 	params := &dynamodb.ScanInput{
 		TableName: aws.String("Test"), // Required
 		AttributesToGet: []*string{
 			aws.String("id"), // Required
 		},
-		ConditionalOperator:    aws.String("ConditionalOperator"),
-		ConsistentRead:         aws.Bool(true),
-		IndexName:              aws.String("IndexName"),
-		Limit:                  aws.Int64(1),
-		ProjectionExpression:   aws.String("ProjectionExpression"),
-		ReturnConsumedCapacity: aws.String("ReturnConsumedCapacity"),
-		Segment:                aws.Int64(1),
-		Select:                 aws.String("Select"),
-		TotalSegments:          aws.Int64(1),
+		Limit: aws.Int64(1),
 	}
 	resp, err := db.Scan(params)
 

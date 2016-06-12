ff8859ce75c4a2b18566a99e0bf8dcabde2fe5ac
Johnzeng <Johnz@misfit.com>
2016-06-12 17:36:17 +0800

    update a put test

--- src/DynamodbAPI_test.go
+++ src/DynamodbAPI_test.go
@@ -6,7 +6,7 @@ import "github.com/aws/aws-sdk-go/service/dynamodb"
 import "github.com/aws/aws-sdk-go/aws/session"
 import "github.com/aws/aws-sdk-go/aws"
 
-func TestDynamoGetTableAPI(t *testing.T) {
+func TestDynamoScanAPI(t *testing.T) {
 
 	db := dynamodb.New(session.New(
 		&aws.Config{
@@ -18,9 +18,42 @@ func TestDynamoGetTableAPI(t *testing.T) {
 		AttributesToGet: []*string{
 			aws.String("id"), // Required
 		},
-		Limit: aws.Int64(1),
+		Limit: aws.Int64(100),
 	}
 	resp, err := db.Scan(params)
+
+	if err != nil {
+		// Print the error, cast err to awserr.Error to get the Code and
+		// Message from an error.
+		fmt.Println(err.Error())
+		return
+	}
+
+	// Pretty-print the response data.
+	fmt.Println(resp)
+}
+
+func TestDynamoPutAPI(t *testing.T) {
+
+	db := dynamodb.New(session.New(
+		&aws.Config{
+			Region:   aws.String("us-east-1"),
+			Endpoint: aws.String("http://127.0.0.1:8000")}))
+
+	params := &dynamodb.PutItemInput{
+		Item: map[string]*dynamodb.AttributeValue{ // Required
+			"id": { // Required
+				S: aws.String("thisisId"),
+			},
+			"tet": {
+				S: aws.String("value"),
+			},
+			// More values...
+		},
+		TableName:    aws.String("Test"), // Required
+		ReturnValues: aws.String("ReturnValue"),
+	}
+	resp, err := db.PutItem(params)
 
 	if err != nil {
 		// Print the error, cast err to awserr.Error to get the Code and

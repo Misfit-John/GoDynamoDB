70caec0aa42a21f9dba742118106a934a981355a
Johnzeng <Johnz@misfit.com>
2016-06-14 15:37:19 +0800

    add a logger

---
+++ main.go
@@ -0,0 +1,14 @@
+package main
+
+import "./src"
+import "os"
+import "io/ioutil"
+
+func main() {
+	GoDynamoDB.Init(ioutil.Discard, os.Stdout, os.Stdout, os.Stderr)
+
+	GoDynamoDB.Trace.Println("I have something standard to say")
+	GoDynamoDB.Info.Println("Special Information")
+	GoDynamoDB.Warning.Println("There is something you need to know about")
+	GoDynamoDB.Error.Println("Something has failed")
+}
